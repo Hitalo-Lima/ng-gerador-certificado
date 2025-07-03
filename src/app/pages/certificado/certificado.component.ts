805e30f93cbfc15b53a3a271f1de8a655dfc5bc2
Hitalo-Lima <hitalolima173@gmail.com>
2025-07-03 12:11:27 -0300

    feat: add certificado page/component with new SVG logo and background images

---
+++ src/app/pages/certificado/certificado.component.ts
@@ -0,0 +1,12 @@
+import { SecondaryButtonComponent } from './../../_components/secondary-button/secondary-button.component';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-certificado',
+  imports: [SecondaryButtonComponent],
+  templateUrl: './certificado.component.html',
+  styleUrl: './certificado.component.css'
+})
+export class CertificadoComponent {
+
+}
