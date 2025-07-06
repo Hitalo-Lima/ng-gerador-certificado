1acab81cb0c496960475eea1654a33036eb2ec82
Hitalo-Lima <hitalolima173@gmail.com>
2025-07-06 11:23:02 -0300

    feat: update routing and component structure by integrating router outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./_components/navbar/navbar.component";
-import { PrimaryButtonComponent } from './_components/primary-button/primary-button.component';
-import { SecondaryButtonComponent } from './_components/secondary-button/secondary-button.component';
-import { ItemCertificadoComponent } from "./_components/item-certificado/item-certificado.component";
-import { BaseUiComponent } from "./_components/base-ui/base-ui.component";
-import { CertificadosComponent } from "./pages/certificados/certificados.component";
-import { CertificadoFormComponent } from "./pages/certificado-form/certificado-form.component";
-import { CertificadoComponent } from "./pages/certificado/certificado.component";
+import { NavbarComponent } from './_components/navbar/navbar.component';
+import { BaseUiComponent } from './_components/base-ui/base-ui.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, BaseUiComponent, CertificadosComponent, CertificadoFormComponent, CertificadoComponent],
+  imports: [NavbarComponent, BaseUiComponent, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'gerador-certificado';
