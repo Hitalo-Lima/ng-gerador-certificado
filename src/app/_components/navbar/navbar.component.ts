4a716523f98fcbf4a6fa9ba34b56b1db313c41b5
Hitalo-Lima <hitalolima173@gmail.com>
2025-07-06 12:05:16 -0300

    feat: update navbar and certificado component to use routerLink for navigation

--- src/app/_components/navbar/navbar.component.ts
+++ src/app/_components/navbar/navbar.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
-export class NavbarComponent implements OnInit{
+export class NavbarComponent implements OnInit {
   ngOnInit(): void {
-    console.log('meu componente navbar inicializou')
+    console.log('meu componente navbar inicializou');
   }
 }
--- src/app/pages/certificado/certificado.component.ts
+++ src/app/pages/certificado/certificado.component.ts
@@ -1,12 +1,11 @@
+import { RouterLink } from '@angular/router';
 import { SecondaryButtonComponent } from './../../_components/secondary-button/secondary-button.component';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-certificado',
-  imports: [SecondaryButtonComponent],
+  imports: [SecondaryButtonComponent, RouterLink],
   templateUrl: './certificado.component.html',
-  styleUrl: './certificado.component.css'
+  styleUrl: './certificado.component.css',
 })
-export class CertificadoComponent {
-
-}
+export class CertificadoComponent {}
