bb39194cf58a4a3e135a1368a88659c5fa446d53
Hitalo-Lima <hitalolima173@gmail.com>
2025-07-07 16:33:21 -0300

    feat:  enhance certificado form with two-way data binding and dynamic activity list

--- src/app/pages/certificado-form/certificado-form.component.ts
+++ src/app/pages/certificado-form/certificado-form.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
-import { SecondaryButtonComponent } from "../../_components/secondary-button/secondary-button.component";
-import { PrimaryButtonComponent } from "../../_components/primary-button/primary-button.component";
+import { SecondaryButtonComponent } from '../../_components/secondary-button/secondary-button.component';
+import { PrimaryButtonComponent } from '../../_components/primary-button/primary-button.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [SecondaryButtonComponent, PrimaryButtonComponent],
+  imports: [SecondaryButtonComponent, PrimaryButtonComponent, FormsModule],
   templateUrl: './certificado-form.component.html',
-  styleUrl: './certificado-form.component.css'
+  styleUrl: './certificado-form.component.css',
 })
 export class CertificadoFormComponent {
-
+  nome: string = '';
+  atividade: string = '';
+  atividades: string[] = ['Angular', 'React'];
 }
