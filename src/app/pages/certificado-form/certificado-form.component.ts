193fa06e346471a2ade2b11053146a98822fad57
Hitalo-Lima <hitalolima173@gmail.com>
2025-07-08 15:48:57 -0300

    feat: improve form validation and error handling in certificado form

--- src/app/pages/certificado-form/certificado-form.component.ts
+++ src/app/pages/certificado-form/certificado-form.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { SecondaryButtonComponent } from '../../_components/secondary-button/secondary-button.component';
 import { PrimaryButtonComponent } from '../../_components/primary-button/primary-button.component';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [SecondaryButtonComponent, PrimaryButtonComponent, FormsModule],
+  imports: [
+    SecondaryButtonComponent,
+    PrimaryButtonComponent,
+    FormsModule,
+    CommonModule,
+  ],
   templateUrl: './certificado-form.component.html',
   styleUrl: './certificado-form.component.css',
 })
@@ -14,4 +19,8 @@ export class CertificadoFormComponent {
   nome: string = '';
   atividade: string = '';
   atividades: string[] = ['Angular', 'React'];
+
+  campoInvalido(control: NgModel) {
+    return control.invalid && control.touched;
+  }
 }
